702f4ef02ed7282cd6795ef7214a4293ed717808
emilaa <emilaa@code.edu.az>
2022-10-02 21:09:40 +0400

    Homework

---
+++ ASP.NET Core - Practice/ASP.NET Core _ Practice/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core___Practice.Controllers
+{
+    public class ContactController
+    {
+        public string Index()
+        {
+            return "Contact page";
+        }
+    }
+}
